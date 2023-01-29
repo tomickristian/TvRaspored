922b3e9e365a2fed4bc1eb32c81e3a893cf583f1
Kristian Tomić <50842457+tomickristian@users.noreply.github.com>
2023-01-29 20:29:35 +0100

    Add files via upload

---
+++ wwwroot/js/CustomScript.js
@@ -0,0 +1,10 @@
+﻿function dodajForm(id) {
+    var element = document.getElementById(id);
+    element.style.display = 'block';
+}
+
+function odustaniForm(id)
+{
+    var element = document.getElementById(id);
+    element.style.display = 'none';
+}
---
+++ wwwroot/js/PretplataScript.js
@@ -0,0 +1,18 @@
+﻿var pretplataButton = document.getElementById('pretplataButton');
+var pretplataInput = document.getElementById('pretplataInput');
+function pretplata() {
+    if (pretplataInput.getAttribute('value') == 1)
+    {
+        pretplataButton.classList.remove('btn-secondary');
+        pretplataButton.classList.add('btn-success');
+        pretplataInput.setAttribute('value', 0);
+        pretplataButton.innerHTML = 'Pretplati Se';
+    }
+    else
+    {
+        pretplataButton.classList.remove('btn-success');
+        pretplataButton.classList.add('btn-secondary');
+        pretplataInput.setAttribute('value', 1);
+        pretplataButton.innerHTML = 'Odjavi Se';
+    }
+}
